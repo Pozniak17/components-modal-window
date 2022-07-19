691424aee64195145d6a9e999cbdea01e493ca84
Yevhenii Pozniak <jekamanu@gmail.com>
2022-07-19 09:50:05 +0300

    add js

--- js/modal.js
+++ js/modal.js
@@ -8,12 +8,43 @@ console.log(refs.backdrop);
 console.log(refs.btnOpen);
 
 // При натисканні GET TOUCH відкривається модалка
-refs.btnOpen.addEventListener("click", toggleModal);
+refs.btnOpen.addEventListener("click", onOpenModal);
 
 // При натисканні хрестик відкривається модалка
-refs.btnClose.addEventListener("click", toggleModal);
+refs.btnClose.addEventListener("click", onCloseModal);
 
-// тогл класу backdrop--hidden
-function toggleModal() {
-  refs.backdrop.classList.toggle("backdrop--hidden");
+// тогл класу backdrop--hidden, альтернатива onOpenModal/onCloseModal
+// function toggleModal() {
+//   refs.backdrop.classList.toggle("backdrop--hidden");
+// }
+
+// клік по бекдропу, закриває модалку
+refs.backdrop.addEventListener("click", onBackdropClick);
+
+// ця функція вішається на бекдроп, якщо бекдроп === місце кліку, то закривай модалку
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+
+// Функція відкривання модалки. Якщо модалка відкрита, слухаємо подію
+function onOpenModal() {
+  window.addEventListener("keydown", onEscKeyPress);
+  refs.backdrop.classList.remove("backdrop--hidden");
+}
+
+// Коли модалка закривається, знімаємо слухача подій
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  refs.backdrop.classList.add("backdrop--hidden");
+}
+
+// Тільки при натисканні Escape закривається модалка
+function onEscKeyPress(event) {
+  const ESC_KEY = event.code === "Escape";
+
+  if (ESC_KEY) {
+    onCloseModal();
+  }
 }
